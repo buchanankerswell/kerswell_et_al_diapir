6dc7a86b62f751df6b5986e484c053d3016f1245
Buchanan Kerswell <BuchananKerswell@boisestate.edu>
2020-08-16 17:45:56 -0600

    restructured to only print data distributions in one and two dimensions

--- dem.R
+++ dem.R
@@ -1,25 +1,13 @@
-load('marx.RData')
 source('functions.R')
+
+# Load data and glimpse its structure
+load('marx.RData')
 
 marx
 glimpse(marx)
 
-# Build object by adding layers
-# l.kmean ........... k-means (assume k = # of clusters)
-# l.bic ............. Bayesian Information Criterion
-# l.gmm ............. Gaussian Mixture Model (assume G = # of components)
-# l.gmm.da .......... GMM discriminant analysis (supervised classification)
-# l.gmm.dr .......... GMM dimension reduction
-# l.den ............. GMM layer suited for density plots
-# Add layers of visualizations
-# l.oned ............ Visualize 1D data
-# l.twod ............ Visualize 2D data
-# l.gif ............. creates gifs of marker position ('xy') and PT
-#
-# Summarise by faceting plots
-# f.summary ........ compiles selected gifs into one
-
-marx1 <- marx %>%
+# Visualize data distribution in 1- and 2D
+vis1 <- marx %>%
   mutate(
     # 1D vis plots
     vis.oned = mark.ft %>%
@@ -35,57 +23,19 @@ marx1 <- marx %>%
         features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
         xlim = c(-2, 2),
         dlim = 0.05
-      ),
-    # BIC
-    bic.mod0 = mark.ft %>%
-      l.bic(
-        features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
-        scale = TRUE,
-        plot = FALSE
-      ),
-    # General GMM model
-    mod0 = l.gmm(
-      lst = mark.ft,
-      lst.bic = bic.mod0,
-      features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
-      G = NULL,
-      scale = TRUE,
-      hists = FALSE,
-      scatter = FALSE
-    ),
-    # Dimension Reduction
-    dr.mod0 = l.gmm.dr(mod0),
-    # BIC
-    bic.mod1 = mark.ft %>%
-      l.bic(
-        G = 3,
-        features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
-        scale = TRUE,
-        plot = FALSE
-      ),
-    # General GMM model
-    mod1 = l.gmm(
-      lst = mark.ft,
-      lst.bic = bic.mod1,
-      features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
-      G = NULL,
-      scale = TRUE,
-      hists = FALSE,
-      scatter = FALSE
-    ),
-    # Dimension Reduction
-    dr.mod1 = l.gmm.dr(mod1)
+      )
   )
+
 # Open PDF for plotting
 cairo_pdf('ft.PDF',
           width = 11,
           height = 8.5,
           onefile = TRUE)
 # Explore features for individual models
-marx1$vis.oned
-marx1$vis.twod
+print(vis1$vis.oned[[1]])
+print(vis1$vis.twod[[1]][[1]])
 # Summarise features by faceting
-marx1$mark.ft %>%
+vis1$mark.ft %>%
   f.oned(
     runs = 'all',
     nrow = 2,
@@ -93,21 +43,81 @@ marx1$mark.ft %>%
     features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
     plot = 'all',
     xlim = c(-2, 2),
-    dlim = 1
-  )
-# Plot BIC
-marx1$bic.mod0 %>% p.BIC()
-marx1$bic.mod1 %>% p.BIC()
-# Plot scatter
-marx1$mod0 %>% p.class()
-marx1$mod1 %>% p.class()
-# Dimension reduction plots
-marx1$dr.mod0 %>% p.dr.boundary()
-marx1$dr.mod1 %>% p.dr.boundary()
+    dlim = 0.5
+  ) %>%
+  print()
+
+# Build models by adding layers
+# l.kmean ........... k-means (assume k = # of clusters)
+# l.bic ............. Bayesian Information Criterion
+# l.gmm ............. Gaussian Mixture Model (assume G = # of components)
+# l.gmm.da .......... GMM discriminant analysis (supervised classification)
+# l.gmm.dr .......... GMM dimension reduction
+# l.den ............. GMM layer suited for density plots
+# Add layers of visualizations
+# l.oned ............ Visualize 1D data
+# l.twod ............ Visualize 2D data
+# l.gif ............. creates gifs of marker position ('xy') and PT
+#
+# Summarise by faceting plots
+# f.summary ........ compiles selected gifs into one
+
+# mods1 <- marx %>%
+#   mutate(
+    # # BIC
+    # bic.mod0 = mark.ft %>%
+    #   l.bic(
+    #     features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
+    #     scale = TRUE,
+    #     plot = FALSE
+    #   ),
+    # # General GMM model
+    # mod0 = l.gmm(
+    #   lst = mark.ft,
+    #   lst.bic = bic.mod0,
+    #   features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
+    #   G = NULL,
+    #   scale = TRUE,
+    #   hists = FALSE,
+    #   scatter = FALSE
+    # ),
+    # # Dimension Reduction
+    # dr.mod0 = l.gmm.dr(mod0),
+    # # BIC
+    # bic.mod1 = mark.ft %>%
+    #   l.bic(
+    #     G = 3,
+    #     features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
+    #     scale = TRUE,
+    #     plot = FALSE
+    #   ),
+    # # General GMM model
+    # mod1 = l.gmm(
+    #   lst = mark.ft,
+    #   lst.bic = bic.mod1,
+    #   features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
+    #   G = NULL,
+    #   scale = TRUE,
+    #   hists = FALSE,
+    #   scatter = FALSE
+    # ),
+    # # Dimension Reduction
+    # dr.mod1 = l.gmm.dr(mod1)
+  # )
+# # Visualize model parameters and results
+# # Plot BIC
+# mods1$bic.mod0 %>% p.BIC()
+# mods1$bic.mod1 %>% p.BIC()
+# # Plot scatter
+# mods1$mod0 %>% p.class()
+# mods1$mod1 %>% p.class()
+# # Dimension reduction plots
+# mods1$dr.mod0 %>% p.dr.boundary()
+# mods1$dr.mod1 %>% p.dr.boundary()
 # Turn off PDF
 dev.off()
 # # Summarise features by animation
-# marx1$mark.ft %>%
+# mods1$mark.ft %>%
 #   a.oned(
 #     plot = 'all',
 #     features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),
@@ -118,7 +128,7 @@ dev.off()
 #     fname = 'oned',
 #     fps = 20
 #   )
-# marx1$mark.ft %>%
+# mods1$mark.ft %>%
 #   a.twod(
 #     type = 'gif',
 #     features = c('dTSum', 'dPSum', 'dTUpSum', 'dPUpSum'),

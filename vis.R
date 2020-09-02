9499db591f66c3f4393bf6dcfc1210a581293fa7
buchanankerswell <buck.kerswell@gmail.com>
2020-09-02 13:27:36 -0600

    just vis mod2 and mod3

--- vis.R
+++ vis.R
@@ -12,16 +12,16 @@ cairo_pdf('vis2.PDF', width = 11, height = 8.5, onefile = TRUE)
 print(mods$vis.oned[[1]])
 print(mods$vis.oned[[2]])
 # Summarise features by faceting
-marx$mark.ft %>%
-  f.oned(
-    nrow = 2,
-    ncol = 2,
-    features = 'all',
-    xlim = c(-2, 3),
-    dlim = 0.3,
-    alpha.min = 0.02
-  ) %>%
-  print()
+# marx$mark.ft %>%
+#   f.oned(
+#     nrow = 2,
+#     ncol = 2,
+#     features = 'all',
+#     xlim = c(-2, 3),
+#     dlim = 0.3,
+#     alpha.min = 0.02
+#   ) %>%
+#   print()
 marx$mark.ft %>%
   f.oned(
     nrow = 2,
@@ -32,68 +32,78 @@ marx$mark.ft %>%
     alpha.min = 0.02
   ) %>%
   print()
+marx$mark.ft %>%
+  f.oned(
+    nrow = 2,
+    ncol = 2,
+    features = c('tsteps', 'tMax'),
+    xlim = c(-2, 3),
+    dlim = 0.3,
+    alpha.min = 0.02
+  ) %>%
+  print()
 # visualize model parameters and results
 # Plot BIC
-mods$bic.mod0 %>% p.BIC()
-mods$bic.mod1 %>% p.BIC()
+# mods$bic.mod0 %>% p.BIC()
+# mods$bic.mod1 %>% p.BIC()
 mods$bic.mod2 %>% p.BIC()
 mods$bic.mod3 %>% p.BIC()
 # Plot scatter
 # mods$mod0 %>% p.class()
 # mods$mod1 %>% p.class()
 # 1D facet plots
-f.oned(
-  mods$mark.ft,
-  mods$mod0,
-  nrow = 2,
-  ncol = 2,
-  features = features,
-  plot = 'ridge',
-  xlim = c(-2, 3),
-  dlim = 0.3,
-  alpha.min = 0
-) %>%
-  print()
-f.oned(
-  mods$mark.ft,
-  mods$dr.mod0,
-  mod.type = 'dr',
-  dr = TRUE,
-  nrow = 2,
-  ncol = 2,
-  features = features,
-  plot = 'ridge',
-  xlim = NULL,
-  dlim = 0.3,
-  alpha.min = 0
-) %>%
-  print()
-f.oned(
-  mods$mark.ft,
-  mods$mod1,
-  nrow = 2,
-  ncol = 2,
-  features = features,
-  plot = 'ridge',
-  xlim = c(-2, 3),
-  dlim = 0.3,
-  alpha.min = 0
-) %>%
-  print()
-f.oned(
-  mods$mark.ft,
-  mods$dr.mod1,
-  mod.type = 'dr',
-  dr = TRUE,
-  nrow = 2,
-  ncol = 2,
-  features = features,
-  plot = 'ridge',
-  xlim = NULL,
-  dlim = 0.3,
-  alpha.min = 0
-) %>%
-  print()
+# f.oned(
+#   mods$mark.ft,
+#   mods$mod0,
+#   nrow = 2,
+#   ncol = 2,
+#   features = features,
+#   plot = 'ridge',
+#   xlim = c(-2, 3),
+#   dlim = 0.3,
+#   alpha.min = 0
+# ) %>%
+#   print()
+# f.oned(
+#   mods$mark.ft,
+#   mods$dr.mod0,
+#   mod.type = 'dr',
+#   dr = TRUE,
+#   nrow = 2,
+#   ncol = 2,
+#   features = features,
+#   plot = 'ridge',
+#   xlim = NULL,
+#   dlim = 0.3,
+#   alpha.min = 0
+# ) %>%
+#   print()
+# f.oned(
+#   mods$mark.ft,
+#   mods$mod1,
+#   nrow = 2,
+#   ncol = 2,
+#   features = features,
+#   plot = 'ridge',
+#   xlim = c(-2, 3),
+#   dlim = 0.3,
+#   alpha.min = 0
+# ) %>%
+#   print()
+# f.oned(
+#   mods$mark.ft,
+#   mods$dr.mod1,
+#   mod.type = 'dr',
+#   dr = TRUE,
+#   nrow = 2,
+#   ncol = 2,
+#   features = features,
+#   plot = 'ridge',
+#   xlim = NULL,
+#   dlim = 0.3,
+#   alpha.min = 0
+# ) %>%
+#   print()
 f.oned(
   mods$mark.ft,
   mods$mod2,
@@ -137,50 +147,50 @@ f.oned(
 dev.off()
 
 # visualise fit
-f.gif(
-  mods$data.tidy,
-  mods$mod0,
-  runs = 'cde46',
-  GIF = 'PT',
-  ncol = 2,
-  nrow = 2,
-  grads = TRUE,
-  save = TRUE,
-  fname = 'vis2.mod0'
-)
-f.gif(
-  mods$data.tidy,
-  mods$dr.mod0,
-  runs = 'cde46',
-  GIF = 'PT',
-  ncol = 2,
-  nrow = 2,
-  grads = TRUE,
-  save = TRUE,
-  fname = 'vis2.dr.mod0'
-)
-f.gif(
-  mods$data.tidy,
-  mods$mod1,
-  runs = 'cde46',
-  GIF = 'PT',
-  ncol = 2,
-  nrow = 2,
-  grads = TRUE,
-  save = TRUE,
-  fname = 'vis2.mod1'
-)
-f.gif(
-  mods$data.tidy,
-  mods$dr.mod1,
-  runs = 'cde46',
-  GIF = 'PT',
-  ncol = 2,
-  nrow = 2,
-  grads = TRUE,
-  save = TRUE,
-  fname = 'vis2.dr.mod1'
-)
+# f.gif(
+#   mods$data.tidy,
+#   mods$mod0,
+#   runs = 'cde46',
+#   GIF = 'PT',
+#   ncol = 2,
+#   nrow = 2,
+#   grads = TRUE,
+#   save = TRUE,
+#   fname = 'vis2.mod0'
+# )
+# f.gif(
+#   mods$data.tidy,
+#   mods$dr.mod0,
+#   runs = 'cde46',
+#   GIF = 'PT',
+#   ncol = 2,
+#   nrow = 2,
+#   grads = TRUE,
+#   save = TRUE,
+#   fname = 'vis2.dr.mod0'
+# )
+# f.gif(
+#   mods$data.tidy,
+#   mods$mod1,
+#   runs = 'cde46',
+#   GIF = 'PT',
+#   ncol = 2,
+#   nrow = 2,
+#   grads = TRUE,
+#   save = TRUE,
+#   fname = 'vis2.mod1'
+# )
+# f.gif(
+#   mods$data.tidy,
+#   mods$dr.mod1,
+#   runs = 'cde46',
+#   GIF = 'PT',
+#   ncol = 2,
+#   nrow = 2,
+#   grads = TRUE,
+#   save = TRUE,
+#   fname = 'vis2.dr.mod1'
+# )
 f.gif(
   mods$data.tidy,
   mods$mod2,
@@ -192,17 +202,17 @@ f.gif(
   save = TRUE,
   fname = 'vis2.mod2'
 )
-f.gif(
-  mods$data.tidy,
-  mods$dr.mod2,
-  runs = 'cde46',
-  GIF = 'PT',
-  ncol = 2,
-  nrow = 2,
-  grads = TRUE,
-  save = TRUE,
-  fname = 'vis2.dr.mod2'
-)
+# f.gif(
+#   mods$data.tidy,
+#   mods$dr.mod2,
+#   runs = 'cde46',
+#   GIF = 'PT',
+#   ncol = 2,
+#   nrow = 2,
+#   grads = TRUE,
+#   save = TRUE,
+#   fname = 'vis2.dr.mod2'
+# )
 f.gif(
   mods$data.tidy,
   mods$mod3,

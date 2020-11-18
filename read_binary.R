24eed6abc1b422b259b864968e39f7d9e1c3d8dc
buchanankerswell <buck.kerswell@gmail.com>
2020-11-17 17:37:29 -0700

    update read binary bug fix

--- read_binary.R
+++
@@ -1,8 +0,0 @@
-# rm(list = ls())
-
-dirs.list <- list.dirs('/Volumes/My Passport/numerical_models/common_depth_2017_fixed_geotherms/dataset', recursive = F)
-dirs <- dirs.list[grepl('*km', dirs.list)]
-    
-
-# Close connection
-# close(f.prn)
